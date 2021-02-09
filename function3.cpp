bd87db30373c8a02b8a2ada7e3ac98f4f8834f69
youssef <youssefelsharbatly@aucegypt.edu>
2021-02-10 01:32:38 +0400

    ysharbatly edits

---
+++ function3.cpp
@@ -0,0 +1,19 @@
+bool solveNQUtil(int board[N][N], int col)
+{
+	
+	if (col >= N)
+		return true;
+
+
+	for (int i = 0; i < N; i++) {
+
+		if (isSafe(board, i, col)) {
+			board[i][col] = 1;
+
+			if (solveNQUtil(board, col + 1))
+				return true;
+			board[i][col] = 0; 
+		}
+	}
+	return false;
+}
---
+++ function4.cpp
@@ -0,0 +1,16 @@
+#include "function2.h"
+bool solveNQ()
+{
+	int board[N][N] = { { 0, 0, 0, 0 },
+						{ 0, 0, 0, 0 },
+						{ 0, 0, 0, 0 },
+						{ 0, 0, 0, 0 } };
+
+	if (solveNQUtil(board, 0) == false) {
+		printf("Solution does not exist");
+		return false;
+	}
+
+	printSolution(board);
+	return true;
+}
