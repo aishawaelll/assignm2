65bbf6e36fc295ff14557250868a1c1d7f643295
Aisha <aishawaelll@aucegypt.edu>
2021-02-09 23:45:22 +0200

    adding main file

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "function1.h"
+#include "function2.h"
+#include "function3.h"
+#include "function4.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	solveNQ();
+	return 0;
+}
